c8891426003e117c68d1e8002b1d4ecdddd91102
Shuaib Memon <ShuaibM@ais.ac.nz>
2019-02-22 09:22:08 +1300

    Version 2 - Shuaib Memon - 22 Feb 2019

--- Test.java
+++ Test.java
@@ -3,7 +3,7 @@ public class Test
 	public static void main(String[] args)
 	{	
 	int tableNo=0;
-	for(int i = 1; i <= 10; i++)
+	for(int i = 1; i <= 5; i++)
 System.out.println(tableNo + " * " + i + " = " + tableNo *i);
 	}	
 }
