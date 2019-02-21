80335586a56704a4a36a7d610b13ce584a159acb
Shuaib Memon <ShuaibM@ais.ac.nz>
2019-02-22 09:08:49 +1300

    Version 1 - Shuaib 22 Feb

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test
+{
+	public static void main(String[] args)
+	{	
+	int tableNo=0;
+	for(int i = 1; i <= 10; i++)
+System.out.println(tableNo + " * " + i + " = " + tableNo *i);
+	}	
+}
+
